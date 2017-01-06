7f409ad6fef0267ff02cb753d2022b169b353a0e
Cao Pao Lo <caoanh.2406.1995@gmail.com>
2017-01-06 14:14:49 +0700

    6/1

--- Multivendor/Model/ResourceModel/Vendor/Collection.php
+++ Multivendor/Model/ResourceModel/Vendor/Collection.php
@@ -1,6 +1,7 @@
 <?php
-namespace Magestore\Multivendor\Model\ResourceModel;
+namespace Magestore\Multivendor\Model\ResourceModel\Vendor;
 class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection {
+    protected $_idFieldName = 'vendor_id';
     protected function _construct(){
         parent::_construct();
         $this->_init('Magestore\Multivendor\Model\Vendor','Magestore\Multivendor\Model\ResourceModel\Vendor');
