d7b3c027b2398b7ce45ab96d93df6caab16e39d1
Cao Pao Lo <caoanh.2406.1995@gmail.com>
2017-01-06 11:01:51 +0700

    error load vendor view

--- Multivendor/Controller/Adminhtml/Vendor/Index.php
+++ Multivendor/Controller/Adminhtml/Vendor/Index.php
@@ -3,14 +3,14 @@ namespace Magestore\Multivendor\Controller\Adminhtml\Vendor;
 class Index extends \Magestore\Multivendor\Controller\Adminhtml\Vendor
 {
     protected $resultPageFactory;
-//    public function __construct(
-//            \Magento\Backend\App\Action\Context $context,
-//            \Magento\Framework\View\Result\PageFactory $resultPageFactory) {
-//        parent::__construct($context);
-//        $this->resultPageFactory = $resultPageFactory;
-//    }
+    public function __construct(
+            \Magento\Backend\App\Action\Context $context,
+            \Magento\Framework\View\Result\PageFactory $resultPageFactory) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
     public function execute() {
-        echo 'paul';exit();
+
         $resultPage = $this->resultPageFactory->create();
         return $resultPage;
     }
